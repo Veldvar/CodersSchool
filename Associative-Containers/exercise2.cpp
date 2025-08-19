b1257817b97bb22d2d24cdab89ccfbf553212d0a
Grzegorz <Veldvar@gmail.com>
2025-08-19 20:51:37 +0200

    Add process uts

--- Associative-Containers/exercise2.cpp
+++ Associative-Containers/exercise2.cpp
@@ -3,6 +3,7 @@
 #include<numeric>
 #include<cmath>
 #include<functional>
+#include<cassert>
 
 enum class ErrorCode{
     
@@ -11,31 +12,74 @@ enum class ErrorCode{
     BadFormat,
     DivideBy0,
     SqrtOfNegativeNumber,
-    ModuleOfNonIntegerValue };
+    ModuleOfNonIntegerValue,
+    None 
+    };
 
+ErrorCode process(std::string input, double* out){ 
     
-
-int main(){
-
     std::map<char, std::function<double(double,double)>> core ={
         {'+',[](double a, double b){return a+b;}},
         {'-',[](double a, double b){return a-b;}},
         {'/',[](double a, double b){return a/b;}},
         {'*',[](double a, double b){return a*b;}},
         {'%',[](double a, double b){return static_cast<int>(a) % static_cast<int>(b);}},
-        // {"!",[](int a, int b){return a!b;}},
+        // {"!",[](int a, int b){return ;}},
         {'^',[](double a, double b){return std::pow(a,b);}}
-        };
+    };
 
-        double x,y;
-        char op;
+    // std::cout << core[op](x,y)<<'\n';
+    // *out =10.0; //sztuczna
+    return ErrorCode::Ok;
+}
+void run_tests(){
+    std::cout<<"Run tests\n";
+    //Test #1 
+    {   
+        // given
+        double out;
+        ErrorCode error_code;
+        std::string input{"5 + 5"};
+        // when
+        error_code = process(input, &out);
+        // then
+        assert(error_code == ErrorCode::Ok);
+        assert(std::fabs(out -10.0 )<0.001);
+    }
+    
+    // //Test #2 
+    // {   
+    //     // given
+    //     double out;
+    //     ErrorCode error_code;
+    //     std::string input{"5 ^ 2"};
+    //     // when
+    //     error_code = procces(input, &out);
+    //     // then
+    //     assert(error_code == ErrorCode::Ok);
+    //     assert(std::fabs(out -25.0 )<0.001);
+    // }
+    std::cout<< "Tests passed\n";
+}
+
+void run_application(){
+    double x,y;
+    char op;
 
         while(true){
-        std::cout <<"Please insert numbers\n";
+        std::cout <<"Please insert numbers:";
+    
+        // std::cout <<"\n";
+    
         std::cin>> x>> op>> y;
+    
+        // IsBadCharacter(op);
+    
        
-        std::cout <<"Please insert numbers:" << core[op](x,y)<<'\n';
-        
-        
         }
+}
+
+int main(){
+    run_tests();
+    // run_application();
 }
